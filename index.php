ce832d41e0112b1634e7ed37c5d513058933cb68
Kevin <kevinkr@aijvanger.nl>
2015-05-27 23:26:16 +0200

    This is the main page.
    This is the main page, but same code applies to ALL other pages that you ever make that needs to have a login validation.

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include( './loginvalidation.php') ?>
+<!-- this PHP include should be the first line in your HTML or PHP document! DO NOT WRITE ANYTHING ABOVE!!! -->
+<!DOCTYPE html>
+<html>
+<head>
+  <!-- head content goes here -->
+</head>
+<body>
+  <!-- body content goes here -->
+</body>
+</html>
