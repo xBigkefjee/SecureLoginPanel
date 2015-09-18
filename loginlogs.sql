f93a706ea1087816c0a889f59efb1ad8701e4d91
Kevin <kevinkr@aijvanger.nl>
2015-09-18 15:22:32 +0200

    Delete loginlogs.sql

--- loginlogs.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE `test`.`loginlogs_test` ( `id` INT NOT NULL AUTO_INCREMENT , `username` VARCHAR(255) NOT NULL , `password` VARCHAR(255) NOT NULL , `time` VARCHAR(255) NOT NULL , `date` VARCHAR(255) NOT NULL , `useragent` VARCHAR(255) NOT NULL , `ip` VARCHAR(255) NOT NULL , `proxy` VARCHAR(255) NOT NULL , `errormsg` VARCHAR(255) NOT NULL ), PRIMARY KEY (`id`) ENGINE = InnoDB;
