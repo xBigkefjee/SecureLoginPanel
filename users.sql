4d7d7deca989694bb47798b5d71f46e7161fd56b
Kevin <kevinkr@aijvanger.nl>
2015-09-18 15:22:20 +0200

    Delete users.sql

--- users.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE `test`.`loginlogs_test` ( `id` INT NOT NULL AUTO_INCREMENT , `username` VARCHAR(255) NOT NULL , `password` VARCHAR(255) NOT NULL , `name` VARCHAR(255) NOT NULL , PRIMARY KEY (`id`), UNIQUE (`username`)) ENGINE = InnoDB;
